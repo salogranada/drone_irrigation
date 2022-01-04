e0965e181bd61c98ca1d84a316a858f6e749da30
salogranada <s.granada@uniandes.edu.co>
2022-01-03 21:45:47 -0500

    updated full data with new calculations

--- src/scripts_ML/features_files_joiner.py
+++ src/scripts_ML/features_files_joiner.py
@@ -7,10 +7,10 @@ total_features_df = pd.DataFrame()
 
 for filename in os.listdir("../data_base/paths_energy/"):
     #Change 'startswith' or 'endswith' depending on what data you want to join
-    if filename.startswith("features_pathprueba"): 
+    if filename.startswith("feat_"): 
         print(filename)
         energy_df = pd.read_csv('../data_base/paths_energy/' + filename, sep=",")
         total_features_df = total_features_df.append(energy_df, ignore_index=True)
         #print(total_features_df)
 
-total_features_df.to_csv('../data_base/paths_energy/joined_features.csv') #Change name
+total_features_df.to_csv('../data_base/paths_energy/joined_features_11y12.csv') #Change name
